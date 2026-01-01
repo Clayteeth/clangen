e4fbfec6220eaa82c7f9c0668170e681dfb4105b
scribblecrumb <abspence2199@gmail.com>
2025-12-31 20:37:44 -0600

    code for longfur apps

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -14,10 +14,13 @@ from scripts.utility import adjust_list_text
 
 class Pelt:
     # POSES
-    all_poses = list(sprites.POSE_DATA["poses"].keys())
+    all_poses = sprites.POSE_DATA["poses"]
     newborn_poses = [x for x in all_poses if "newborn" in x]
     kitten_poses = [x for x in all_poses if "kitten" in x]
-    adolescent_poses = [x for x in all_poses if "adolescent" in x]
+    adolescent_long_poses = [x for x in all_poses if "adolescent_long" in x]
+    adolescent_short_poses = [
+        x for x in all_poses if "adolescent" in x and "long" not in x
+    ]
     adult_short_poses = [x for x in all_poses if "adult_short" in x and "para" not in x]
     adult_long_poses = [x for x in all_poses if "adult_long" in x and "para" not in x]
     senior_poses = [x for x in all_poses if "senior" in x]
@@ -264,6 +267,7 @@ class Pelt:
                 para_adult_sprite,
             ]
         ):
+            # DO NOT CHANGE THIS: this is meant to convert old saves and should not be updated with new pose additions
             self.cat_sprites = {
                 "kitten": kitten_sprite if kitten_sprite is not None else 0,
                 "adolescent": adol_sprite if adol_sprite is not None else 3,
@@ -337,6 +341,12 @@ class Pelt:
                 else "adult_short0"
             )
 
+            if adol_sprite in ("adolescent0", "adolescent1", "adolescent2"):
+                if self.length == "long":
+                    adol_sprite = random.choice(self.adolescent_long_poses)
+                else:
+                    adol_sprite = f"adolescent_short{adol_sprite[-1]}"
+
             self.cat_sprites = {
                 "newborn": newborn_sprite
                 if newborn_sprite is not None and newborn_sprite in self.newborn_poses
@@ -345,8 +355,12 @@ class Pelt:
                 if kitten_sprite is not None and kitten_sprite in self.kitten_poses
                 else "kitten0",
                 "adolescent": adol_sprite
-                if adol_sprite is not None and adol_sprite in self.adolescent_poses
-                else "adolescent0",
+                if adol_sprite is not None
+                and (
+                    adol_sprite in self.adolescent_short_poses
+                    or adol_sprite in self.adolescent_long_poses
+                )
+                else "adolescent_short0",
                 "young adult": adult_sprite,
                 "adult": adult_sprite,
                 "senior adult": adult_sprite,
@@ -821,7 +835,6 @@ class Pelt:
         self.cat_sprites = {
             "newborn": random.choice(self.newborn_poses),
             "kitten": random.choice(self.kitten_poses),
-            "adolescent": random.choice(self.adolescent_poses),
             "senior": random.choice(self.senior_poses),
             "para_young": "para_young0",
         }
@@ -830,6 +843,11 @@ class Pelt:
         self.skin = choice(Pelt.skin_sprites)
 
         if self.length == "long":
+            self.cat_sprites["adolescent"] = random.choice(
+                self.adolescent_long_poses
+                if self.adolescent_long_poses
+                else self.adolescent_short_poses
+            )
             self.cat_sprites["adult"] = random.choice(
                 self.adult_long_poses
                 if self.adult_long_poses
@@ -837,6 +855,7 @@ class Pelt:
             )
             self.cat_sprites["para_adult"] = "para_adult_long0"
         else:
+            self.cat_sprites["adolescent"] = random.choice(self.adolescent_short_poses)
             self.cat_sprites["adult"] = random.choice(self.adult_short_poses)
             self.cat_sprites["para_adult"] = "para_adult_short0"
 
