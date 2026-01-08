68420ad6ec0919fdfd86b4c8422b6e897834a91f
scribblecrumb <abspence2199@gmail.com>
2026-01-08 16:38:21 -0600

    add adult prioritization to random_cat allocation

--- scripts/events_module/patrol/patrol.py
+++ scripts/events_module/patrol/patrol.py
@@ -260,9 +260,20 @@ class Patrol:
         # DETERMINE RANDOM CAT
         # Find random cat
         if len(patrol_cats) > 1:
-            self.random_cat = choice(
-                [i for i in patrol_cats if i != self.patrol_leader]
-            )
+            # prioritize grabbing an adult as the random cat
+            if self.patrol_statuses.get("normal adult") > 1:
+                self.random_cat = choice(
+                    [
+                        i
+                        for i in self.patrol_cats
+                        if i != self.patrol_leader and i not in self.patrol_apprentices
+                    ]
+                )
+            # if no adults, grab anyone
+            else:
+                self.random_cat = choice(
+                    [i for i in patrol_cats if i != self.patrol_leader]
+                )
         else:
             self.random_cat = choice(patrol_cats)
 
