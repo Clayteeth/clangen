0f20c6031a347b8511fd177661fab6d43dc3bc15
scribblecrumb <abspence2199@gmail.com>
2026-02-12 17:02:14 -0600

    formatter

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -2776,7 +2776,7 @@ class Cat:
                             respect=rel["respect"] or 0,
                             comfort=rel["comfort"] or 0,
                             trust=rel["trust"] or 0,
-                            log=rel["log"]
+                            log=rel["log"],
                         )
                         self.relationships[rel["cat_to_id"]] = new_rel
 
